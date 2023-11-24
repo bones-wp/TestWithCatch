ec0c1bd4940ad5af375a448a4814b088c74e3e4a
Osmolovskiy Yuriy <osmolovskiy.yuriy@ya.ru>
2023-11-24 11:12:44 +0300

    Initial commit

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,5 +1,24 @@
-package PACKAGE_NAME;/**
-* @author Professional
-* @since 24.11.2023
-*/public class Utils {
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * @author Osmolovkiy Yuriy
+ * @since 24.11.2023
+ */
+public class Utils {
+    public static void tetsWithCatch(TestRunnable runnable, Class<? extends Exception> exceptionClass) {
+        try {
+            runnable.run();
+            fail("Должна быть выброшена ошибка " + exceptionClass);
+        } catch (Exception e) {
+            if (!Objects.equals(e.getClass(), exceptionClass)) {
+                fail("Выброшена ошибка " + e.getClass() + " но ожидается " + exceptionClass);
+            }
+        }
+    }
+
+    public interface TestRunnable {
+        void run() throws Exception;
+    }
 }
